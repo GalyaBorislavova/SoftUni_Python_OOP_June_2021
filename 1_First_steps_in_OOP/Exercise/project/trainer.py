38118a1bbd0fe4f88f1e8616ec400a2c759d8c96
Galya Borislavova <galyaborislavova888@gmail.com>
2021-07-07 16:54:41 +0300

    change parameter in add_pokemon function

--- 1_First_steps_in_OOP/Exercise/project/trainer.py
+++ 1_First_steps_in_OOP/Exercise/project/trainer.py
@@ -7,21 +7,21 @@ class Trainer:
         self.name = name
         self.pokemons = []
 
-    def add_pokemon(self, pokemon: Pokemon):
+    def add_pokemon(self, pokemon_to_add: Pokemon):
         if pokemon in self.pokemons:
             return "This pokemon is already caught"
-        self.pokemons.append(pokemon)
-        return "Caught " + Pokemon.pokemon_details(pokemon)
+        self.pokemons.append(pokemon_to_add)
+        return "Caught " + Pokemon.pokemon_details(pokemon_to_add)
 
     def release_pokemon(self, pokemon_name: str):
-        for pokemon in self.pokemons:
-            if pokemon_name == pokemon.name:
-                self.pokemons.remove(pokemon)
+        for p in self.pokemons:
+            if pokemon_name == p.name:
+                self.pokemons.remove(p)
                 return f"You have released {pokemon_name}"
         return "Pokemon is not caught"
 
     def trainer_data(self):
-        pokemon_details = [f'- {Pokemon.pokemon_details(pokemon)}' for pokemon in self.pokemons]
+        pokemon_details = [f'- {Pokemon.pokemon_details(p)}' for p in self.pokemons]
         output = f"Pokemon Trainer {self.name}" + "\n" + f"Pokemon count {len(self.pokemons)}" + "\n"
         return output + '\n'.join(pokemon_details)
 
