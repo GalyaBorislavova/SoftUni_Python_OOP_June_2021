4c46a5b6b84b6193ed840a63ba6a38aa30d7b94e
Galya Borislavova <galyaborislavova888@gmail.com>
2021-07-14 10:19:00 +0300

    exercise 3

---  5_Static_and_Class_Methods/Lab/3_integer.py
+++  5_Static_and_Class_Methods/Lab/3_integer.py
@@ -14,7 +14,39 @@ def romanToInt(s):
     s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
     for char in s:
         number += translations[char]
-    print(number)
+    return number
 
 
-romanToInt('MCMIV')
+class Integer:
+    def __init__(self, value):
+        self.value = value
+
+    @classmethod
+    def from_float(cls, float_value):
+        if isinstance(float_value, float):
+            return cls(int(float_value))
+        return "value is not a float"
+
+    @classmethod
+    def from_roman(cls, value):
+        return cls(romanToInt(value))
+
+    @classmethod
+    def from_string(cls, value):
+        if isinstance(value, str):
+            try:
+                string_to_int = int(value)
+                if isinstance(string_to_int, int):
+                    return cls(string_to_int)
+            except Exception:
+                return "wrong type"
+        return "wrong type"
+
+
+if __name__ == "__main__":
+    first_num = Integer(10)
+    second_num = Integer.from_roman("IV")
+
+    print(Integer.from_float("2.6"))
+    print(Integer.from_string(2.6))
+
