9f76b48f212ac6d41fbe403874f931a65ceb42d4
Galya Borislavova <galyaborislavova888@gmail.com>
2021-07-29 11:13:28 +0300

    add way 2 for __next__

--- 8_Iterators_and_Generators/Exercise/sequence_repeat.py
+++ 8_Iterators_and_Generators/Exercise/sequence_repeat.py
@@ -7,15 +7,25 @@ class sequence_repeat:
     def __iter__(self):
         return self
 
+    #  way 1
+    # def __next__(self):
+    #     if self.number == 0:
+    #         raise StopIteration
+    #     self.number -= 1
+    #     if self.index >= len(self.sequence):
+    #         self.index = 0
+    #     item = self.sequence[self.index]
+    #     self.index += 1
+    #     return item
+
+    # way 2
     def __next__(self):
-        if self.number == 0:
+        if self.index < self.number:
+            ind = self.index
+            self.index += 1
+            return self.sequence[ind % len(self.sequence)]
+        else:
             raise StopIteration
-        self.number -= 1
-        if self.index >= len(self.sequence):
-            self.index = 0
-        item = self.sequence[self.index]
-        self.index += 1
-        return item
 
 
 result = sequence_repeat('abc', 5)
